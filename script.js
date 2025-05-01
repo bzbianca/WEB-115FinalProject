cc0a55d7d8935321607b24b5318a8766b9519d4a
bzbianca <bzbaccay@my.waketech.edu>
2025-05-01 13:43:04 -0400

    continue

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ let taskTable = document.getElementById("taskTable")
 let id = 0; // id num of task
 let tasks = [] // object task list
 let taskTracker = [] // track which task is which as an array
+
+submit.addEventListener("click", function(){
+    let taskName = document.getElementById("taskName")
+    if (taskName.validity.valueMissing) {
+        taskName.setCustomValidity("Enter a task name.")
+    } else {
+        taskName.setCustomValidity("")
+    }
+})
+
+
 submit.addEventListener("click", function(){
     // create a new blank task object
     const task = {
@@ -17,10 +28,10 @@ submit.addEventListener("click", function(){
         date: null,
     }
     
-    // let date = new Date()
-    // getMonth()
-    // getDay()
-    // getYear()
+    // create date
+    let addDate = new Date()
+    addDate = `${addDate.getMonth()}/${addDate.getDay()}/${addDate.getFullYear()}`
+
     // gets all input needed for creating task
     let taskName = document.getElementById("taskName")
     let priority = document.getElementById("priority")
@@ -28,7 +39,6 @@ submit.addEventListener("click", function(){
     
     id += 1
     taskTracker.push(id)
-    console.log(taskTracker)
 
     // add all input into task object
     task.id = id;
@@ -36,65 +46,82 @@ submit.addEventListener("click", function(){
     task.priority = priority.value;
     task.isImportant = important.checked
     tasks.push(JSON.stringify(task))
-    console.log(JSON.stringify(tasks));
 
     // ---adding task to table---
-    newTask = document.createElement("tr") // create row task
+    let newTask = document.createElement("tr") // create row task
+    newTask.id = id
 
     // add information
-    makeTaskName = document.createElement("td") // create task name element
-    makeTaskName.id = "task" + String(id)
+    let makeTaskName = document.createElement("td") // create task name element
     makeTaskName.innerHTML = taskName.value
+    makeTaskName.id = id
     newTask.appendChild(makeTaskName)
 
-    makePriority = document.createElement("td") // create task name element
+    let makePriority = document.createElement("td") // create task priority element
     makePriority.innerHTML = priority.value
+    makePriority.id = id
     newTask.appendChild(makePriority)
 
-    
+    let makeDate = document.createElement("td") // create task date element
+    makeDate.innerHTML = addDate
+    makeDate.id = id
+    newTask.appendChild(makeDate)
 
-    makeOptions = document.createElement("td") // for both the completion and delete buttons
-    completeButton = document.createElement("input") // create complete checkbox
+
+    let makeOptions = document.createElement("td") // for both the completion and delete buttons
+    
+    let addStatus = document.createElement("p")
+    addStatus.innerHTML = "Done"
+    addStatus.id = id
+    makeOptions.appendChild(addStatus)
+    
+    let completeButton = document.createElement("input") // create complete checkbox
     completeButton.type = "checkbox" // makes input into a checkbox
     completeButton.innerHTML = "Done"
-    // completeButton.id = "complete" + String(id) // gives button an id
+    completeButton.id = "complete" + String(id) // gives button an id
     makeOptions.appendChild(completeButton)
 
-    deleteButton = document.createElement("button")
+    let deleteButton = document.createElement("button")
     deleteButton.type = "button"
     deleteButton.innerHTML = "Delete"
     makeOptions.appendChild(deleteButton)
+    newTask.appendChild(makeOptions)
 
     if (priority.value == "High") {
         newTask.style = "background-color: red;"
-        // newTask.style = "border-color: red;"
     }
 
-    newTask.appendChild(makeOptions)
+    // check to see if the user checked if task is complete
+    let doneOrNot = false;
+    completeButton.addEventListener("change", function(){
+        if (doneOrNot) {
+            document.getElementById(newTask.id).style.textDecoration = "none"
+            addStatus.innerHTML = "Done"
+            doneOrNot = false
+        } else {
+            document.getElementById(newTask.id).style.textDecoration = "line-through"
+            addStatus.innerHTML = "Undo"
+            doneOrNot = true
+        }
+    })
 
-    taskTable.appendChild(newTask)
+    // if the delete button is clicked on specific task
+    deleteButton.addEventListener("click", function(){
+        document.getElementById(newTask.id).remove()
+        // console.log(tasks.indexOf(newTask.id))
+        // for (let i = 0; i <= tasks.length; i++) {
+        //     if (i == tasks[i]) {
+
+        //     }
+        // }
+        console.log(tasks)
+    })
+
+    taskTable.appendChild(newTask) // add task to table
+
+    console.log(JSON.stringify(tasks)); // print objects array in console
 })
 
-
-    
-
-// completeButton = document.createElement("input") // create complete checkbox
-// completeButton.type = "checkbox" // makes input into a checkbox
-// completeButton.textContent = "Done"
-// completeButton.id = "complete" + String(id) // gives button an id
-// taskmanager.append(completeButton)
-// completeButton.addEventListener("change", function(){
-//     console.log("helo")
-// })
-
-// how to delete individual/its own row
-// how to space cells
-// how to add text next to checkbox
-// MAKE SURE TO ADD THE DATE
-
-// checkboxes and how they work - how to check if it is checked or not
-// check to see how to select a child's parent for tasks
-// strikethrough (while use background-Color: red for each task for importance)
-// how to add alerts like the credit card assignment MAKE SURE TO PREVENT INVALID INPUT SUCH AS EMPTY TASK NAMES DO THIS WITH AN ALERT
+// add removing task object from list
 
 // make sure to do readme file
